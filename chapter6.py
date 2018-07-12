c956d4b364b635f16bda15d2b27685f174df9e54
Jaeki Hong <jaeki.hong@gmail.com>
2018-07-12 18:21:44 +0900

    chap6 update

--- chapter6.py
+++ chapter6.py
@@ -1,4 +1,4 @@
-def f6_2(n):
+def f2(n):
     count =1
     for i in range(n):
         for j in range(i+1):
@@ -7,11 +7,11 @@ def f6_2(n):
         print()
 
 #f6_2(3)
-def f6_6(matrix):
+def f6(matrix):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if i==j:
                 print(matrix[i][j])
 
 #f6_6([[1,0],[0,1]])
-f6_6([[1,2,3],[4,5,6],[7,8,9]])
+#f6_6([[1,2,3],[4,5,6],[7,8,9]])
