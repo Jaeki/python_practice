bd0b76b89c98ef9be38a2027560b2a2a405ec6bd
Jaeki Hong <jaeki.hong@gmail.com>
2018-07-18 12:28:00 +0900

    #4 updated

--- chapter14-A.py
+++ chapter14-A.py
@@ -34,10 +34,31 @@ def second_largest(lst):
             second_largest = lst[i]
     return print(second_largest)
 
-second_largest([3,-2,10,5])
-second_largest([-2,1,1,-3,5])
-second_largest([1,2,3,3])
-second_largest(["alpha","gamma","beta","delta"])
-second_largest([3.1,3.1])
-second_largest([True,False, False, True])
-second_largest([False, False, True])
+# second_largest([3,-2,10,5])
+# second_largest([-2,1,1,-3,5])
+# second_largest([1,2,3,3])
+# second_largest(["alpha","gamma","beta","delta"])
+# second_largest([3.1,3.1])
+# second_largest([True,False, False, True])
+# second_largest([False, False, True])
+
+# print french for the numbers between lo and hi (inclusive)
+def print_french(lo, hi):
+    return None
+
+def digit(num, pos):
+    return (num // 10**(pos-1)) % 10
+
+def num_in_french(num): # assumes 0 <= num <= 100
+
+    ones_list = ["zero", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf", "dix", "onze", "douze", "treize",
+                 "quatorze", "quinze", "seize", "dix-sept", "dix-huit", "dix-neuf"]
+
+    tens_list = ["", "dix", "vingt", "trente", "quarante", "cinquante", "soixante", "soixante", "quatre-vingt", "quatre-vingt"]
+
+    # Part 1: get the ones and tens digits of num
+    # Part 2: fill in code below for numbers 1, 2, 3, ..., 19 and 100
+    # Part 4: case when the numbers are 70, 71, 72, ..., 79 and 90, 91, 92, ..., 99
+    # Part 5: otherwise the case when the numbers are 20, 30, 40, ...
+    # Part 6: otherwise the case when the numbers are 21, 31, 41, ...
+    # Part 7: everything else, the most general case for 22, 23, ..., 29, 32,
