1bb7f72024bbde0a02961c52157367755c88bdb1
Jaeki Hong <jaeki.hong@gmail.com>
2018-07-20 17:18:58 +0900

    updated db scheme

--- DB_term_project/db_scheme.sql
+++ DB_term_project/db_scheme.sql
@@ -1,23 +1,38 @@
 create table Building(
-    id int auto_increment Primary key,
-    name varchar(200) not null,
-    location varchar(200) not null,
-    capacity int,
-    check (capacity >= 1));
+   BID int auto_increment primary key,
+    BName varchar(200) not null,
+    BLocation varchar(200) not null,
+    BMax int,
+    check (BMax >= 1));
 
-create table Performance(
-    id int auto_increment Primary key,
-    name varchar(200),
-    type varchar(200),
-    price int,
+create table Performance (
+   PID int auto_increment primary key,
+    PName varchar(200),
+    PType varchar(200),
+    PPrice int,
     check (PPrice >= 0)
 );
 
 create table Audience(
-    id int auto_increment Primary key,
-    name varchar(200),
-    gender char(1),
-    age int,
-    check (gender in ('M', 'F')),
-    check (age >=1)
+   AID int auto_increment Primary key,
+    AName varchar(200),
+    AGender char(1),
+    AAge int ,
+    check (AGender in ('M', 'F')),
+    check (AAge >=1)
 );
+
+create table Booking(
+   PID int ,
+    AID int ,
+    SeatNo int,
+    foreign key (PID) references Performance on delete cascade,
+    foreign key (AI) references Audience on delete cascade
+);
+
+create table Assign(
+   PID int,
+    AID int,
+    foreign key (PID) references Performance on delete cascade,
+    foreign key (AI) references Audience on delete cascade
+)
