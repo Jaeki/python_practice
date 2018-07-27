4fcd71214634ffba8bf6596c02584707f91ec07a
sktmfrkd <41132069+sktmfrkd@users.noreply.github.com>
2018-07-27 19:01:28 +0900

    Update chapter10B.py

--- chapter10B.py
+++ chapter10B.py
@@ -1,7 +1,7 @@
 # for range를 이용해서 range를 이용하기
 def f2(n):
     print_value = 1
-    for i in range(n+1):
+    for i in range(n):
         print_value += i
         print(list(map(lambda x : x+print_value , range(i+1))))
 
