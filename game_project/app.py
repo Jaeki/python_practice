836c49ebcd366a922935b1f34b815d47b4fe146b
konlo <konlona@gmail.com>
2018-09-20 19:02:11 +0900

    Update app.py

--- game_project/app.py
+++ game_project/app.py
@@ -12,22 +12,53 @@ class App(Frame):
 
         # TODO
         # Conveyor 객체 생성
-        self.conveyor = Conveyor(master, self.resized_images, num)
+        self.app_frame = Frame(master)
+
+        self.app_frame.grid(row=0, column=0)
+
+        self.Conveyer_frame = Frame(self.app_frame)
+        self.Conveyer_frame.grid(row=3, column=0)
+
+        self.conveyor = Conveyor(self, self.resized_images, num)
         
         # TODO
         # MainTable 객체 생성
-        self.table = Maintable(master, self.alphabet_images, self.figure_images , num)
-        
+        self.Maintable_frame = Frame(self.app_frame)
+        self.Maintable_frame.grid(row=0, column=0)
+
+        self.table = Maintable(self, self.alphabet_images, self.figure_images , num)
+
+
     def load_images(self):
-        self.figure_images = list(Image.open("picture\\%d.JPG" % (i+1)) for i in range(self.num*self.num))
+        self.figure_images = list(Image.open("picture\\%d.JPG" % (i+1)) for i in range(self.num*self.num + 1))
         self.alphabet_images = list(PhotoImage(file="alphabet\\%d.GIF" % (i+1)) for i in range(self.num*self.num))
         self.resized_images = list(ImageTk.PhotoImage(self.figure_images[i].resize((50,50), Image.ANTIALIAS)) for i in range(self.num*self.num))
-        self.figure_images = list(ImageTk.PhotoImage(self.figure_images[i]) for i in range(self.num*self.num))
+        self.figure_images = list(ImageTk.PhotoImage(self.figure_images[i]) for i in range(self.num*self.num + 1))
 
     # TODO
     # MainTable에서 선택한 도형 이미지와 Conveyor에서 Marker가 현재 가리키는 이미지를 비교한 후 비교 결과에 따라 처리한다.
-    def compare_images(self):        
-        pass
+    def compare_images(self, clicked_index):
+
+        print("convery index", self.conveyor.cur_idx)
+        print("clicked index", clicked_index)
+        # 이미지가 일치하지 않는 경우
+
+        # convery의 현재 index와 clicked index가 같은 경우는 맞음
+        if( self.conveyor.cur_idx == clicked_index):
+            # success
+            if(self.conveyor.correct_match_config() == 1):
+                self.app_frame.grid_forget()
+                self.app_frame.pack_forget()
+
+                b = ImageButton(self.master, image=self.figure_images[16], relief=SOLID, overrelief=RIDGE, borderwidth=1)
+                b.grid(column=0, row=0)
+                b.bind("<Button-1>", sys.exit)
+
+                print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+        else :
+            self.conveyor.wrong_match_config()
+        print("compare_images")
     # TODO
     # 종료 조건 만족 시 실행
     def finish(self, win):       
